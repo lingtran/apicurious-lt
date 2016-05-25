865aad6bc38b083d1f9c780943cf38dcbb86f523
lingtran <lxiutran@gmail.com>
2016-05-25 16:26:56 -0600

    Add capability to accept new user tokens more dynamically

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,17 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      @profile = Profile.find_by(current_user.name)
-      @starred = Starred.find_by(current_user.name)
-      @owner_repos = Repositories.find_owner_repos(current_user.name)
-      @member_repos = Repositories.find_member_repos(current_user.name)
-      @all_repos = Repositories.find_all_sorted(current_user.name)
+      @profile = Profile.find_by(current_user)
+      # @starred = Starred.find_by(current_user.name)
+      # @owner_repos = Repositories.find_owner_repos(current_user.name)
+      # @member_repos = Repositories.find_member_repos(current_user.name)
+      # @all_repos = Repositories.find_all_sorted(current_user.name)
     else
       @profile = Profile.new
+      # @starred = Starred.new
+      # @owner_repos = Repositories.new
+      # @member_repos = Repositories.new
+      # @all_repos = Repositories.new
     end
   end
 
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,15 +1,43 @@
 class Profile < OpenStruct
-  def self.service
-    GithubService.new
+
+  def self.service(user)
+    GithubService.new(user.oauth_token)
   end
 
-  def self.find_by(login)
-    temp_profile_hash = service.profile_hash(login)
+  def self.find_by(user)
+    temp_profile_hash = Profile.service(user).profile_hash(user.screen_name)
     Profile.new(temp_profile_hash)
   end
 
-  def join_date(login)
-    creation = Profile.find_by(login).created_at.to_time
+  def join_date(user)
+    creation = Profile.find_by(user).created_at.to_time
     creation.strftime("Joined on %b %d, %Y")
   end
+
+  def starred_count(user)
+     starred = Profile.service(user).starred_hash(user.screen_name)
+     starred.length
+  end
+
+  def owner_repos(user)
+    temp_owner_repos_arr = Profile.service(user).owner_repos_hash(user.screen_name)
+    temp_owner_repos_arr.map do |owner_repo_hash|
+      Profile.new(owner_repo_hash)
+    end
+  end
+
+  def member_repos(user)
+    temp_member_repos_arr = Profile.service(user).member_repos_hash(user.screen_name)
+    temp_member_repos_arr.map do |member_repo_hash|
+      Profile.new(member_repo_hash)
+    end.take(5)
+  end
+
+  def all_sorted(user)
+    temp_repos_arr = Profile.service(user).sorted_repos_with_contributions_hash(user.screen_name)
+    temp_repos_arr.map do |repo_hash|
+      Profile.new(repo_hash)
+    end
+  end
+
 end
